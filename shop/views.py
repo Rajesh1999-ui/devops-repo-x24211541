9c5f5b3588ff2d174ddd3fd5a87d97ad51ad60f1
EC2 Default User <ec2-user@ip-172-31-66-231.ec2.internal>
2026-06-24 09:02:35 +0000

    changes

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.core.paginator import Paginator
-
+# from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from shop.models import Product, Category
 from cart.forms import QuantityForm
 
